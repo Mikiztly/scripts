4c36c077bb9e5ec9f374bbb7c702071f6f773c7c
Diego Casavilla <diegocasavilla@gmail.com>
2024-03-18 11:19:16 -0300

    Agregue script para reinciar el contador del RDP en windows

---
+++ windows/reinicio_rdp.cmd
@@ -0,0 +1,11 @@
+REM El servicio RDP de Windows tiene un periodo de prueba de 180 días, con esto se borra el valor
+REM del registro que cuenta esos días y lo vuelve a 0, HAY QUE REINICIAR EL SERVIDOR para que tome los cambios
+REM como el script esta pensado para que se ejecute automaticamente no muestro mensajer por consola
+
+REM Lo primero es borrar la entrada en el registro
+reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\RCM\GracePeriod" /v *
+
+REM Ahora fuerzo el reinicio del servidor
+shutdown /r /f
+
+REM Esto se puede programar cada 175 dias para tener una "licencia permanente" del servidio RDP
